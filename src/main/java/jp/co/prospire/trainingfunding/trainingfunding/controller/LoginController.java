628156e3e2cc8ddecdd6d372a41205dac632c445
genta-aikawa <genta-aikawa@p1.techfirm.co.jp>
2018-10-30 18:42:44 +0900

    ・Springのディレクトリ構成に合わせてHTML素材のパスを移動 ・適当なサンプルページ追加（説明用）

---
+++ src/main/java/jp/co/prospire/trainingfunding/trainingfunding/controller/LoginController.java
@@ -0,0 +1,29 @@
+package jp.co.prospire.trainingfunding.trainingfunding.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model) {
+        return "login";
+    }
+
+    // TODO: サンプル用メソッドなので不要です。
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String home(Model model) {
+        model.addAttribute("login_url", "login");
+        return "home";
+    }
+
+    // TODO: サンプル用メソッドなので不要です。
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return "hello";
+    }
+
+}
